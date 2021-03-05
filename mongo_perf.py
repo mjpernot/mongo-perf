ad5dfbf1fefd51604ae9d3f883d37d43ba0d77ad
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-03-05 13:44:55 +0000

    Updated mongo_perf.run_program v2.3.0 - Added authorization mechanism to the mongo_class.RepSet class instance

--- mongo_perf.py
+++ mongo_perf.py
@@ -309,11 +309,16 @@ def run_program(args_array, func_dict, **kwargs):
         cfg = gen_libs.load_module(args_array["-m"], args_array["-d"])
 
     if server.repset and server.repset_hosts:
+
+        # Only pass authorization mechanism if present.
+        auth_mech = {"auth_mech": server.auth_mech} if hasattr(
+            server, "auth_mech") else {}
+
         mongo = mongo_class.RepSet(
             server.name, server.user, server.japd, host=server.host,
             port=server.port, auth=server.auth, repset=server.repset,
             repset_hosts=server.repset_hosts, auth_db=server.auth_db,
-            use_arg=server.use_arg, use_uri=server.use_uri)
+            use_arg=server.use_arg, use_uri=server.use_uri, **auth_mech)
 
     else:
         mongo = mongo_libs.create_instance(args_array["-c"], args_array["-d"],
