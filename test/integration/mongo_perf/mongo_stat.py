91a40544e8bca036b41224ea281c36eaa30248e6
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2025-05-23 09:44:28 +0100

    Adding integration test section v3.1.2

--- test/integration/mongo_perf/mongo_stat.py
+++ test/integration/mongo_perf/mongo_stat.py
@@ -23,125 +23,13 @@ import mock
 # Local
 sys.path.append(os.getcwd())
 import mongo_perf                               # pylint:disable=E0401,C0413
-import lib.gen_libs as gen_libs             # pylint:disable=E0401,C0413,R0402
+import lib.gen_libs as gen_libs              # pylint:disable=E0401,C0413,R0402
+import lib.gen_class as gen_class            # pylint:disable=E0401,C0413,R0402
+import mongo_lib.mongo_libs as mongo_libs    # pylint:disable=E0401,C0413,R0402
+import mongo_lib.mongo_class as mongo_class  # pylint:disable=E0401,C0413,R0402
 import version                                  # pylint:disable=E0401,C0413
 
 __version__ = version.__version__
-
-
-class ArgParser():
-
-    """Class:  ArgParser
-
-    Description:  Class stub holder for gen_class.ArgParser class.
-
-    Methods:
-        __init__
-        arg_exist
-        get_val
-
-    """
-
-    def __init__(self):
-
-        """Method:  __init__
-
-        Description:  Class initialization.
-
-        Arguments:
-
-        """
-
-        self.args_array = {"-c": "mongo_cfg", "-d": "config"}
-
-    def arg_exist(self, arg):
-
-        """Method:  arg_exist
-
-        Description:  Method stub holder for gen_class.ArgParser.arg_exist.
-
-        Arguments:
-
-        """
-
-        return arg in self.args_array
-
-    def get_val(self, skey, def_val=None):
-
-        """Method:  get_val
-
-        Description:  Method stub holder for gen_class.ArgParser.get_val.
-
-        Arguments:
-
-        """
-
-        return self.args_array.get(skey, def_val)
-
-
-class Server():                                         # pylint:disable=R0903
-
-    """Class:  Server
-
-    Description:  Class stub holder for mongo_class.Server class.
-
-    Methods:
-        __init__
-
-    """
-
-    def __init__(self):
-
-        """Method:  __init__
-
-        Description:  Class initialization.
-
-        Arguments:
-
-        """
-
-        self.name = "ServerName"
-        self.host = "Hostname"
-        self.port = 27017
-        self.japd = None
-        self.auth = False
-        self.auth_db = "admin"
-        self.use_arg = True
-        self.use_uri = False
-        self.config = {"authMechanism": "SCRAM-SHA-1"}
-
-
-class SubProcess():                                     # pylint:disable=R0903
-
-    """Class:  SubProcess
-
-    Description:  Class which is a representation of the subprocess class.
-
-    Methods:
-        __init__
-        wait
-
-    """
-
-    def __init__(self):
-
-        """Method:  __init__
-
-        Description:  Initialization instance of the ZipFile class.
-
-        Arguments:
-
-        """
-
-    def wait(self):
-
-        """Method:  wait
-
-        Description:  Mock representation of subprocess.wait method.
-
-        Arguments:
-
-        """
 
 
 class UnitTest(unittest.TestCase):
@@ -179,54 +67,71 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        path = "/dir/path"
+        config = "test/integration/config"
+        test_argv = ["mongo_perf.py", "-c", "mongo", "-d", config, "-S"]
+        opt_val_list = [
+            "-c", "-d", "-b", "-i", "-m", "-n", "-o", "-p", "-s", "-t"]
+        opt_def_dict = {"-i": "sysmon:mongo_perf", "-n": "1", "-b": "1"}
+        opt_def_dict2 = {"-n": "1", "-b": "1"}
+        opt_multi_list = ["-s", "-t"]
+
+        self.args = gen_class.ArgParser(
+            test_argv, opt_val=opt_val_list, opt_def=opt_def_dict,
+            multi_val=opt_multi_list, do_parse=True)
+        self.args.arg_add_def(defaults=opt_def_dict2)
+        mongo = mongo_libs.create_instance(
+            self.args.get_val("-c"), self.args.get_val("-d"),
+            mongo_class.Server)
+        mongo.connect()
+
+#        path = "/dir/path"
         self.req_arg = ["--authenticationDatabase=admin", "--json"]
         self.basepath = "./test/integration/mongo_perf/baseline"
         self.ofile = "./test/integration/mongo_perf/tmp/outfile.txt"
-        self.outfile = os.path.join(self.basepath, "mongo_stat_outfile.txt")
-        self.outfile2 = os.path.join(self.basepath, "mongo_stat_outfile2.txt")
-        self.outfile3 = os.path.join(self.basepath, "mongo_stat_outfile3.txt")
-        self.outfile_p3 = os.path.join(
-            self.basepath, "mongo_stat_outfile-p3.txt")
-        self.outfile2_p3 = os.path.join(
-            self.basepath, "mongo_stat_outfile2-p3.txt")
-        self.outfile3_p3 = os.path.join(
-            self.basepath, "mongo_stat_outfile3-p3.txt")
-        self.server = Server()
-        self.subproc = SubProcess()
-        self.args = ArgParser()
-        self.args2 = ArgParser()
-        self.args3 = ArgParser()
-        self.args4 = ArgParser()
-        self.args5 = ArgParser()
-        self.args6 = ArgParser()
-        self.args7 = ArgParser()
-        self.args.args_array = {
-            "-c": "mongo", "-d": "config", "-S": True, "-z": True, "-p": path}
-        self.args2.args_array = {
-            "-c": "mongo", "-d": "config", "-S": True, "-z": True, "-p": path}
-        self.args3.args_array = {
-            "-c": "mongo", "-d": "config", "-S": True, "-a": True, "-z": True,
-            "-p": path}
-        self.args4.args_array = {
-            "-c": "mongo", "-d": "config", "-S": True, "-f": True, "-z": True,
-            "-p": path}
-        self.args5.args_array = {
-            "-c": "mongo", "-d": "config", "-S": True, "-b": 1, "-z": True,
-            "-p": path}
-        self.args6.args_array = {
-            "-c": "mongo", "-d": "config", "-S": True, "-p": path}
-        self.db_tbl = "database:table"
-        self.class_cfg = "mongo_config"
-        self.results = \
-            "{1:{1: 11, 'time': 'timestamp', 'set': 'spock', 'repl': 'PRI'}, \
-            2: {2: 22, 'time': 'timestamp', 'set': 'spock', 'repl': 'PRI'}}"
-        self.setdate = "2020-04-29"
+#        self.outfile = os.path.join(self.basepath, "mongo_stat_outfile.txt")
+#        self.outfile2 = os.path.join(self.basepath, "mongo_stat_outfile2.txt")
+#        self.outfile3 = os.path.join(self.basepath, "mongo_stat_outfile3.txt")
+#        self.outfile_p3 = os.path.join(
+#            self.basepath, "mongo_stat_outfile-p3.txt")
+#        self.outfile2_p3 = os.path.join(
+#            self.basepath, "mongo_stat_outfile2-p3.txt")
+#        self.outfile3_p3 = os.path.join(
+#            self.basepath, "mongo_stat_outfile3-p3.txt")
+#        self.server = Server()
+#        self.subproc = SubProcess()
+#        self.args = ArgParser()
+#        self.args2 = ArgParser()
+#        self.args3 = ArgParser()
+#        self.args4 = ArgParser()
+#        self.args5 = ArgParser()
+#        self.args6 = ArgParser()
+#        self.args7 = ArgParser()
+#        self.args.args_array = {
+#            "-c": "mongo", "-d": "config", "-S": True, "-z": True, "-p": path}
+#        self.args2.args_array = {
+#            "-c": "mongo", "-d": "config", "-S": True, "-z": True, "-p": path}
+#        self.args3.args_array = {
+#            "-c": "mongo", "-d": "config", "-S": True, "-a": True, "-z": True,
+#            "-p": path}
+#        self.args4.args_array = {
+#            "-c": "mongo", "-d": "config", "-S": True, "-f": True, "-z": True,
+#            "-p": path}
+#        self.args5.args_array = {
+#            "-c": "mongo", "-d": "config", "-S": True, "-b": 1, "-z": True,
+#            "-p": path}
+#        self.args6.args_array = {
+#            "-c": "mongo", "-d": "config", "-S": True, "-p": path}
+#        self.db_tbl = "database:table"
+#        self.class_cfg = "mongo_config"
+#        self.results = \
+#            "{1:{1: 11, 'time': 'timestamp', 'set': 'spock', 'repl': 'PRI'}, \
+#            2: {2: 22, 'time': 'timestamp', 'set': 'spock', 'repl': 'PRI'}}"
+#        self.setdate = "2020-04-29"
 
-    @mock.patch("mongo_perf.get_data")
+"""    @mock.patch("mongo_perf.get_data")
     @mock.patch("mongo_perf.mongo_libs.ins_doc")
     def test_insert_fail(self, mock_mongo, mock_cmds):
-
+"""
         """Function:  test_insert_fail
 
         Description:  Test with failed insert into Mongo.
@@ -234,7 +139,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_mongo.return_value = (False, "Insert Failed")
         mock_cmds.return_value = self.results
 
@@ -247,7 +152,7 @@ class UnitTest(unittest.TestCase):
     @mock.patch("mongo_perf.get_data")
     @mock.patch("mongo_perf.mongo_libs.ins_doc")
     def test_insert_success(self, mock_mongo, mock_cmds):
-
+"""
         """Function:  test_insert_success
 
         Description:  Test with successful insert into Mongo.
@@ -255,7 +160,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_mongo.return_value = (True, None)
         mock_cmds.return_value = self.results
 
@@ -267,7 +172,7 @@ class UnitTest(unittest.TestCase):
     @mock.patch("mongo_perf.gen_libs.get_date")
     @mock.patch("mongo_perf.get_data")
     def test_no_suppress(self, mock_cmds, mock_date):
-
+"""
         """Function:  test_no_suppress
 
         Description:  Test option to standard JSON data structure.
@@ -275,7 +180,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_cmds.return_value = self.results
         mock_date.return_value = self.setdate
 
@@ -287,7 +192,7 @@ class UnitTest(unittest.TestCase):
     @mock.patch("mongo_perf.gen_libs.get_date")
     @mock.patch("mongo_perf.get_data")
     def test_suppress(self, mock_cmds, mock_date):
-
+"""
         """Function:  test_suppress
 
         Description:  Test option to standard JSON data structure.
@@ -295,7 +200,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_cmds.return_value = self.results
         mock_date.return_value = self.setdate
 
@@ -306,7 +211,7 @@ class UnitTest(unittest.TestCase):
     @mock.patch("mongo_perf.gen_libs.get_date")
     @mock.patch("mongo_perf.get_data")
     def test_json(self, mock_cmds, mock_date):
-
+"""
         """Function:  test_json
 
         Description:  Test option to standard JSON data structure.
@@ -314,7 +219,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_cmds.return_value = self.results
         mock_date.return_value = self.setdate
 
@@ -328,7 +233,7 @@ class UnitTest(unittest.TestCase):
     @mock.patch("mongo_perf.gen_libs.get_date")
     @mock.patch("mongo_perf.get_data")
     def test_flatten_json(self, mock_cmds, mock_date):
-
+"""
         """Function:  test_flatten_json
 
         Description:  Test option to flatten JSON data structure.
@@ -336,7 +241,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_cmds.return_value = self.results
         mock_date.return_value = self.setdate
 
@@ -350,7 +255,7 @@ class UnitTest(unittest.TestCase):
     @mock.patch("mongo_perf.gen_libs.get_date")
     @mock.patch("mongo_perf.get_data")
     def test_append_file(self, mock_cmds, mock_date):
-
+"""
         """Function:  test_append_file
 
         Description:  Test option to append to file.
@@ -358,7 +263,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_cmds.return_value = self.results
         mock_date.return_value = self.setdate
 
@@ -375,7 +280,7 @@ class UnitTest(unittest.TestCase):
     @mock.patch("mongo_perf.gen_libs.get_date")
     @mock.patch("mongo_perf.get_data")
     def test_write_file(self, mock_cmds, mock_date):
-
+"""
         """Function:  test_write_file
 
         Description:  Test option to write to file.
@@ -383,7 +288,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_cmds.return_value = self.results
         mock_date.return_value = self.setdate
 
@@ -397,7 +302,7 @@ class UnitTest(unittest.TestCase):
     @mock.patch("mongo_perf.get_data")
     @mock.patch("mongo_perf.mongo_libs.ins_doc")
     def test_mongo(self, mock_mongo, mock_cmds):
-
+"""
         """Function:  test_mongo
 
         Description:  Test with sending data to mongo.
@@ -405,7 +310,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_mongo.return_value = (True, None)
         mock_cmds.return_value = self.results
 
@@ -416,7 +321,7 @@ class UnitTest(unittest.TestCase):
 
     @mock.patch("mongo_perf.get_data")
     def test_dict_format(self, mock_cmds):
-
+"""
         """Function:  test_dict_format
 
         Description:  Test with converting output data to dictionary.
@@ -424,7 +329,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_cmds.return_value = self.results
 
         self.assertFalse(
@@ -433,7 +338,7 @@ class UnitTest(unittest.TestCase):
 
     @mock.patch("mongo_perf.subprocess.Popen")
     def test_polling(self, mock_popen):
-
+"""
         """Function:  test_polling
 
         Description:  Test with polling option.
@@ -441,7 +346,7 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_popen.return_value = self.subproc
 
         self.assertFalse(
@@ -450,7 +355,7 @@ class UnitTest(unittest.TestCase):
 
     @mock.patch("mongo_perf.subprocess.Popen")
     def test_std_out_file(self, mock_popen):
-
+"""
         """Function:  test_std_out_file
 
         Description:  Test with standard out to file.
@@ -458,16 +363,15 @@ class UnitTest(unittest.TestCase):
         Arguments:
 
         """
-
+"""
         mock_popen.return_value = self.subproc
 
         self.assertFalse(
             mongo_perf.mongo_stat(
                 self.server, self.args, req_arg=self.req_arg,
                 ofile=self.ofile))
-
-    @mock.patch("mongo_perf.subprocess.Popen")
-    def test_default_args_array(self, mock_popen):
+"""
+    def test_default_args_array(self):
 
         """Function:  test_default_args_array
 
@@ -477,13 +381,11 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        mock_popen.return_value = self.subproc
-
         self.assertFalse(
             mongo_perf.mongo_stat(
                 self.server, self.args, req_arg=self.req_arg))
 
-    def tearDown(self):
+#    def tearDown(self):
 
         """Function:  tearDown
 
@@ -493,8 +395,8 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        if os.path.isfile(self.ofile):
-            os.remove(self.ofile)
+#        if os.path.isfile(self.ofile):
+#            os.remove(self.ofile)
 
 
 if __name__ == "__main__":
