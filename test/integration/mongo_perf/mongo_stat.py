f4cf37824c76388d4c6169f81675b7ea79d22fc1
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2025-05-23 11:17:38 +0100

    Adding integration test section v3.1.2

--- test/integration/mongo_perf/mongo_stat.py
+++ test/integration/mongo_perf/mongo_stat.py
@@ -58,14 +58,9 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp
-        test_insert_fail
-        test_insert_success
-        test_no_suppress
-        test_suppress
-        test_json
+        test_expand_json
         test_flatten_json
         test_append_file
-
         test_polling
         test_out_file
         test_no_std_out
@@ -92,6 +87,8 @@ class UnitTest(unittest.TestCase):
         opt_def_dict2 = {"-n": "1", "-b": "1"}
         opt_multi_list = ["-s", "-t"]
         self.opt_arg_list = {"-n": "-n=", "-r": "--tlsInsecure"}
+        self.req_arg = ["--authenticationDatabase=admin", "--json"]
+        self.ofile = "./test/integration/mongo_perf/tmp/outfile.txt"
 
         self.args = gen_class.ArgParser(
             test_argv, opt_val=opt_val_list, opt_def=opt_def_dict,
@@ -116,160 +113,23 @@ class UnitTest(unittest.TestCase):
             mongo_class.Server)
         self.mongo.connect()
 
-#        path = "/dir/path"
-        self.req_arg = ["--authenticationDatabase=admin", "--json"]
-        self.basepath = "./test/integration/mongo_perf/baseline"
-        self.ofile = "./test/integration/mongo_perf/tmp/outfile.txt"
-#        self.outfile = os.path.join(self.basepath, "mongo_stat_outfile.txt")
-#        self.outfile2 = os.path.join(self.basepath, "mongo_stat_outfile2.txt")
-#        self.outfile3 = os.path.join(self.basepath, "mongo_stat_outfile3.txt")
-#        self.outfile_p3 = os.path.join(
-#            self.basepath, "mongo_stat_outfile-p3.txt")
-#        self.outfile2_p3 = os.path.join(
-#            self.basepath, "mongo_stat_outfile2-p3.txt")
-#        self.outfile3_p3 = os.path.join(
-#            self.basepath, "mongo_stat_outfile3-p3.txt")
-#        self.server = Server()
-#        self.subproc = SubProcess()
-#        self.args = ArgParser()
-#        self.args2 = ArgParser()
-#        self.args3 = ArgParser()
-#        self.args4 = ArgParser()
-#        self.args5 = ArgParser()
-#        self.args6 = ArgParser()
-#        self.args7 = ArgParser()
-##        self.args.args_array = {
-##            "-c": "mongo", "-d": "config", "-S": True, "-z": True, "-p": path}
-##        self.args2.args_array = {
-##            "-c": "mongo", "-d": "config", "-S": True, "-z": True, "-p": path}
-#        self.args3.args_array = {
-#            "-c": "mongo", "-d": "config", "-S": True, "-a": True, "-z": True,
-#            "-p": path}
-#        self.args4.args_array = {
-#            "-c": "mongo", "-d": "config", "-S": True, "-f": True, "-z": True,
-#            "-p": path}
-##        self.args5.args_array = {
-##            "-c": "mongo", "-d": "config", "-S": True, "-b": 1, "-z": True,
-##            "-p": path}
-##        self.args6.args_array = {
-##            "-c": "mongo", "-d": "config", "-S": True, "-p": path}
-#        self.db_tbl = "database:table"
-#        self.class_cfg = "mongo_config"
-#        self.results = \
-#            "{1:{1: 11, 'time': 'timestamp', 'set': 'spock', 'repl': 'PRI'}, \
-#            2: {2: 22, 'time': 'timestamp', 'set': 'spock', 'repl': 'PRI'}}"
-#        self.setdate = "2020-04-29"
+    def test_expand_json(self, mock_cmds, mock_date):
 
-    @unittest.skip("Skipping test for now")
-    @mock.patch("mongo_perf.get_data")
-    @mock.patch("mongo_perf.mongo_libs.ins_doc")
-    def test_insert_fail(self, mock_mongo, mock_cmds):
+        """Function:  test_expand_json
 
-        """Function:  test_insert_fail
-
-        Description:  Test with failed insert into Mongo.
+        Description:  Test option to expand JSON data structure.
 
         Arguments:
 
         """
-
-        mock_mongo.return_value = (False, "Insert Failed")
-        mock_cmds.return_value = self.results
-
-        with gen_libs.no_std_out():
-            self.assertFalse(
-                mongo_perf.mongo_stat(
-                    self.server, self.args2, db_tbl=self.db_tbl,
-                    class_cfg=self.class_cfg, req_arg=self.req_arg))
-
-    @unittest.skip("Skipping test for now")
-    @mock.patch("mongo_perf.get_data")
-    @mock.patch("mongo_perf.mongo_libs.ins_doc")
-    def test_insert_success(self, mock_mongo, mock_cmds):
-
-        """Function:  test_insert_success
-
-        Description:  Test with successful insert into Mongo.
-
-        Arguments:
-
-        """
-
-        mock_mongo.return_value = (True, None)
-        mock_cmds.return_value = self.results
-
-        self.assertFalse(
-            mongo_perf.mongo_stat(
-                self.server, self.args2, db_tbl=self.db_tbl,
-                class_cfg=self.class_cfg, req_arg=self.req_arg))
-
-    @unittest.skip("Skipping test for now")
-    @mock.patch("mongo_perf.gen_libs.get_date")
-    @mock.patch("mongo_perf.get_data")
-    def test_no_suppress(self, mock_cmds, mock_date):
-
-        """Function:  test_no_suppress
-
-        Description:  Test option to standard JSON data structure.
-
-        Arguments:
-
-        """
-
-        mock_cmds.return_value = self.results
-        mock_date.return_value = self.setdate
-
-        with gen_libs.no_std_out():
-            self.assertFalse(
-                mongo_perf.mongo_stat(
-                    self.server, self.args6, req_arg=self.req_arg))
-
-    @unittest.skip("Skipping test for now")
-    @mock.patch("mongo_perf.gen_libs.get_date")
-    @mock.patch("mongo_perf.get_data")
-    def test_suppress(self, mock_cmds, mock_date):
-
-        """Function:  test_suppress
-
-        Description:  Test option to standard JSON data structure.
-
-        Arguments:
-
-        """
-
-        mock_cmds.return_value = self.results
-        mock_date.return_value = self.setdate
-
-        self.assertFalse(
-            mongo_perf.mongo_stat(
-                self.server, self.args2, req_arg=self.req_arg))
-
-    @unittest.skip("Skipping test for now")
-    @mock.patch("mongo_perf.gen_libs.get_date")
-    @mock.patch("mongo_perf.get_data")
-    def test_json(self, mock_cmds, mock_date):
-
-        """Function:  test_json
-
-        Description:  Test option to standard JSON data structure.
-
-        Arguments:
-
-        """
-
-        mock_cmds.return_value = self.results
-        mock_date.return_value = self.setdate
 
         mongo_perf.mongo_stat(
-            self.server, self.args2, class_cfg=self.class_cfg,
-            req_arg=self.req_arg, ofile=self.ofile)
+            self.mongo, self.args2, req_arg=self.req_arg,
+            opt_arg=self.opt_arg_list, ofile=self.ofile)
+        self.mongo.disconnect()
 
-        self.assertTrue(filecmp.cmp(self.outfile, self.ofile) or
-                        filecmp.cmp(self.outfile_p3, self.ofile))
+        self.assertGreater(line_cnt(self.ofile), 20)
 
-    @unittest.skip("Skipping test for now")
-    @mock.patch("mongo_perf.gen_libs.get_date")
-    @mock.patch("mongo_perf.get_data")
     def test_flatten_json(self, mock_cmds, mock_date):
 
         """Function:  test_flatten_json
@@ -280,15 +140,12 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        mock_cmds.return_value = self.results
-        mock_date.return_value = self.setdate
-
         mongo_perf.mongo_stat(
-            self.server, self.args4, class_cfg=self.class_cfg,
-            req_arg=self.req_arg, ofile=self.ofile)
+            self.mongo, self.args, req_arg=self.req_arg,
+            opt_arg=self.opt_arg_list, ofile=self.ofile)
+        self.mongo.disconnect()
 
-        self.assertTrue(filecmp.cmp(self.outfile3, self.ofile) or
-                        filecmp.cmp(self.outfile3_p3, self.ofile))
+        self.assertEqual(line_cnt(self.ofile), 1)
 
     def test_append_file(self):
 
